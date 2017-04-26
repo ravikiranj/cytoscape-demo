eec7689b5e5991baf22342bd28a5ac1c938358a6
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2017-04-25 20:57:37 -0700

    Added functions

--- js/test.js
+++ js/test.js
@@ -11,16 +11,53 @@ $(document).ready(function($) {
             },
             { // node b
                 data: {
+                    id: 'b'
+                }
+            },
+            { // node c
+                data: {
                     id: 'c'
                 }
             },
-            { // edge ab
+            { // node d
+                data: {
+                    id: 'd'
+                }
+            },
+            { // edge ac
                 data: {
                     id: 'ac',
                     source: 'a',
-                    target: 'c'
+                    target: 'c',
+                    weight: 50
+                }
+            },
+            { // edge bd
+                data: {
+                    id: 'bd',
+                    source: 'b',
+                    target: 'd',
+                    weight: 50
+                }
+            },
+            { // edge dc
+                data: {
+                    id: 'dc',
+                    source: 'd',
+                    target: 'c',
+                    weight: 50
+                }
+            },
+            { // edge ba
+                data: {
+                    id: 'ba',
+                    source: 'b',
+                    target: 'a',
+                    weight: 1
                 }
             }
+
+
         ],
 
         style: [ // the stylesheet for the graph
@@ -55,4 +92,22 @@ $(document).ready(function($) {
         */
 
     });
+
+    var edges = cy.filter(function(element, index) {
+        return !element.isNode();
+    });
+    var nodes = cy.filter(function(element, index) {
+        return element.isNode();
+    });
+
+    var nodesAndEdges = edges.union(nodes);
+
+    var dijkstra = cy.elements().dijkstra('#a', function(self){
+        return self.data('weight');
+    });
+
+    var pathToD = dijkstra.pathTo(cy.$("#d"));
+    var things = pathToD.map(function(i){return i.id();});
+    console.log(pathToD);
+    console.log(things);
 });
